25cc1e1f9edc76d3fe267885d956f9bfe8ca64a5
LesMiserables1 <liemandre20@gmail.com>
2020-11-12 09:17:25 +0700

    fix some bugs

--- backend/index.js
+++ backend/index.js
@@ -6,77 +6,90 @@ let app = express()
 app.use(express.json())
 app.use(cors())
 
-app.post('/register',(req,res)=>{
+app.post('/register', (req, res) => {
     let body = req.body
     res.send(200)
 })
 
-app.post('/login',async(req,res)=>{
+app.post('/login', async (req, res) => {
     let body = req.body
     let passwordHash = crypto.createHash('sha256').update(body.password).digest('base64')
 
-    const user = await db.user.findOne({where : {username : body.username,password : passwordHash}})
-    if(user == null){
+    const user = await db.user.findOne({ where: { username: body.username, password: passwordHash } })
+    if (user == null) {
         return res.send({
-            "status" : "failed",
-            "message" : "wrong credential" 
+            "status": "failed",
+            "message": "wrong credential"
         })
     }
     let payload = {
-        "username" : body.username
+        "username": body.username
     }
-    const token = jwt.sign(payload,process.env.SECRET_KEY)
+    const token = jwt.sign(payload, process.env.SECRET_KEY)
     return res.send({
-        "status" : "ok",
+        "status": "ok",
         token
     })
 })
 
-app.post('/registertransport', (req, res)=>{
- if (!req.body.nama_courier || !req.body.password || !req.body.email) {
-     return res.send({
-        message: 'Please provide a name, email and a password.',
-     });
-   } else {
-       const newUser = await courier.create({
-         nama_courier: req.body.nama_courier,
-         email: req.body.email,
-         password: req.body.password
-       });
+/* 
+    TODO :
+    1. validasi role client apakah transport atau bukan
+    2. validasi fungsi api
+*/
+app.post('/transport/register', async(req, res) => {
+    if (!req.body.nama_courier || !req.body.password || !req.body.email) {
+        return res.send({
+            message: 'Please provide a name, email and a password.',
+        });
+    } else {
+        const newUser = await courier.create({
+            nama_courier: req.body.nama_courier,
+            email: req.body.email,
+            password: req.body.password
+        });
 
-       return res.send({
-        status: "created",
-           message: "Account created!",
+        return res.send({
+            status: "created",
+            message: "Account created!",
         });
     }
-};
+})
 
-app.post('/logintransport',async(req,res)=>{
+    
+
+
+app.post('/transport/login', async (req, res) => {
+
+    let body = req.body
     let passwordHash = crypto.createHash('sha256').update(req.body.password).digest('base64')
 
-    const user = await courier.findOne({where : {email : body.email, password : passwordHash}})
-    if(user == null){
+    const user = await courier.findOne({ where: { email: body.email, password: passwordHash } })
+    if (user == null) {
         return res.send({
-            "status" : "failed",
-            "message" : "wrong credential" 
+            "status": "failed",
+            "message": "wrong credential"
         })
     }
     let payload = {
-        "email" : body.email
+        "email": body.email
     }
-    const token = jwt.sign(payload,process.env.SECRET_KEY)
+    const token = jwt.sign(payload, process.env.SECRET_KEY)
     return res.send({
-        "status" : "ok",
+        "status": "ok",
         token
     })
 })
 
 
-app.get('/viewshipment', (req, res) => {
+app.get('/shipment/details', (req, res) => {
+
     shipping_details.findAll({
         include: [
             { model: shipping_details, where: { id: req.body.courierId, status: req.body.shipStatus } }
         ]
     })
-    .then(data => res.json(data))
+        .then(data => res.json(data))
 })
+
+app.listen(4000)
