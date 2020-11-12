80210c4e98c0b6c714d36da40bbcf9ba29dec171
sherinedevi <48640817+sherinedevi@users.noreply.github.com>
2020-11-12 08:50:23 +0700

    add functional

--- backend/index.js
+++ backend/index.js
@@ -31,3 +31,52 @@ app.post('/login',async(req,res)=>{
         token
     })
 })
+
+app.post('/registertransport', (req, res)=>{
+ if (!req.body.nama_courier || !req.body.password || !req.body.email) {
+     return res.send({
+        message: 'Please provide a name, email and a password.',
+     });
+   } else {
+       const newUser = await courier.create({
+         nama_courier: req.body.nama_courier,
+         email: req.body.email,
+         password: req.body.password
+       });
+
+       return res.send({
+        status: "created",
+           message: "Account created!",
+        });
+    }
+};
+
+app.post('/logintransport',async(req,res)=>{
+    let passwordHash = crypto.createHash('sha256').update(req.body.password).digest('base64')
+
+    const user = await courier.findOne({where : {email : body.email, password : passwordHash}})
+    if(user == null){
+        return res.send({
+            "status" : "failed",
+            "message" : "wrong credential" 
+        })
+    }
+    let payload = {
+        "email" : body.email
+    }
+    const token = jwt.sign(payload,process.env.SECRET_KEY)
+    return res.send({
+        "status" : "ok",
+        token
+    })
+})
+
+
+app.get('/viewshipment', (req, res) => {
+    shipping_details.findAll({
+        include: [
+            { model: shipping_details, where: { id: req.body.courierId, status: req.body.shipStatus } }
+        ]
+    })
+    .then(data => res.json(data))
+})
