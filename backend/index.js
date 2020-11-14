b94cbfa0b4fde4f2bd2489084a7a6ae7dabf9fdc
LesMiserables1 <liemandre20@gmail.com>
2020-11-14 13:42:49 +0700

    add seller feature

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,15 @@ const cors = require('cors')
 const model = require('./model.js')
 const crypto = require('crypto')
 const jwt = require("jsonwebtoken");
-const verifyToken = require('./verify.js')
+const verifyToken = require('./verify.js');
+const { store } = require('./model.js');
 let app = express()
 app.use(express.json())
 app.use(cors())
 
 
+
+// api untuk customer
 app.post('/customer/register',async(req,res)=>{
     let body = req.body
     let passwordHash = crypto.createHash('sha256').update(body.password).digest('base64')
@@ -68,6 +71,95 @@ app.post('/customer/update',verifyToken,async(req,res)=>{
     })
 })  
 
+app.post('/customer/retrieve/data',verifyToken,async(req,res)=>{
+    
+    if(req.decode.role != 'customer'){
+        res.send({
+            "status" : "failed",
+            "msg" : "role is incorrect"
+        })
+    }
+    const customer = await model.customer.findOne({id : req.decode.id})
+    
+    res.send({
+        "status" : 'ok',
+        "data" : customer
+    })
+})
+
+// api untuk store
+
+app.post('/store/register',async(req,res)=>{
+    let body = req.body
+    let passwordHash = crypto.createHash('sha256').update(body.password).digest('base64')
+
+    const customer = await model.store.create({
+        "email" : body.email,
+        "password" : passwordHash
+    })
+    res.send({
+        status : 'ok'
+    })
+})
+
+app.post('/store/login',async(req,res)=>{
+    let body = req.body
+    let passwordHash = crypto.createHash('sha256').update(body.password).digest('base64')
+
+    const customer = await model.store.findOne({ where: { email: body.email, password: passwordHash } })
+    if (customer == null) {
+        return res.send({
+            "status": "failed",
+            "message": "wrong credential"
+        })
+    }
+    let payload = {
+        "role" : "store",
+        "id" : store.id
+    }
+    const token = jwt.sign(payload, process.env.SECRET_KEY)
+    return res.send({
+        "status": "ok",
+        token
+    })
+})
+
+app.post('/store/update',verifyToken,async(req,res)=>{
+    let body = req.body
+    console.log(req.decode)
+    if(req.decode.role != 'store'){
+        res.send({
+            "status" : "failed",
+            "msg" : "role is incorrect"
+        })
+    }
+    let store = await model.store.findOne({id : req.decode.id})
+    store.alamat = body.alamat
+    store.nama_toko = body.nama_toko
+    store.nomor_hp = body.nomor_hp
+    store.kota = body.kota
+    store.save()
+    
+    res.send({
+        status : "ok",
+    })
+})
+
+app.post('/store/retrieve/data',verifyToken,async(req,res)=>{
+    
+    if(req.decode.role != 'store'){
+        res.send({
+            "status" : "failed",
+            "msg" : "role is incorrect"
+        })
+    }
+    const store = await model.store.findOne({id : req.decode.id})
+    
+    res.send({
+        "status" : 'ok',
+        "data" : store
+    })
+})
 /* 
     TODO :
     1. validasi role client apakah transport atau bukan
