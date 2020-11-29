9c01911f3bde19887e337cdc00e9b5e7fcd84e28
sherinedevi <48640817+sherinedevi@users.noreply.github.com>
2020-11-29 09:28:39 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -454,8 +454,8 @@ app.post('/transport/register', async (req, res) => {
         const transport = await model.courier.create({
             nama_courier: req.body.nama_courier,
             email: req.body.email,
-            password: passwordHash     
-            "alamat": req.body.alamat,
+            password: passwordHash,     
+            alamat: req.body.alamat
             
         })
         return res.send({
@@ -663,7 +663,7 @@ app.post('/customer/create/order', verifyToken, async (req, res) => {
         { where: { id: order.id } }
     )
     return res.send({
-        status: 'ok'
+        status: 'ok',
         "orderId" : order.id
     })
 })
